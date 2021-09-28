7b4c730032cb86d9e1fa6ce22558efe1667f970d
Jason Yapri <jason@inergi.id>
2021-09-28 19:12:31 +0700

    Custom error page

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="error" class="h-screen text-center flex flex-col justify-center items-center">
+    <p id="errorStatus" class="flex justify-center items-center bg-red-600 text-white text-3xl w-56 h-16 mb-2">{{ error.statusCode }}</p>
+    <p id="errorMsg" class="font-bold">{{ error.message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'error_layout',
+  props: ['error']
+}
+</script>
